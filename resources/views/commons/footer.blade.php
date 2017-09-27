9d6a833e16bca656e5cab5b2350d17a9f17ae343
Kohei Komachi <kohei.komachi@gmail.com>
2017-09-27 11:43:50 +0000

    top page

---
+++ resources/views/commons/footer.blade.php
@@ -0,0 +1,30 @@
+<footer class="footer">
+  <div class="container">
+    <p class="text-muted">Place sticky footer content here.</p>
+  </div>
+</footer>
+
+<!--
+<footer>
+    <nav class="footer navbar-inverse navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+            <div class="text-center">
+                <p>copyright@ 20xx - 20xx xxxxxx Privacy Policy</p>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">Administrator</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</footer>
+-->
