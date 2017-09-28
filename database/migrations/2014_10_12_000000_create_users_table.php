76f017d73bb78a4669f163aaea64349772d905c7
Kohei Komachi <kohei.komachi@gmail.com>
2017-09-28 05:11:00 +0000

    user login

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,8 +12,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('user-id');
+        Schema::create('users', function
+        (Blueprint $table) {
+            $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->string('age');
